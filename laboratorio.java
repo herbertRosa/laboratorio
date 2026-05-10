d59f3e9692b5230a7b2f48297d696d7fe26492b3
Herbert <herbert.rs.rs@gmail.com>
2026-05-10 16:20:22 -0300

    Update laboratorio.java

--- laboratorio.java
+++ laboratorio.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class laboratorio {
     public static void main(String[] args) {
 
-        double amostrasDoDia;  //entrada
+        int amostrasDoDia;  //entrada
         double tempoDeProcessamento; //entrada
-        double resultadosValidos; //entrada
+        int resultadosValidos; //entrada
 
         double tempoMedioAmostras; //saida
-        double resultadoTotal; //saida
+        double resultadoProcessadosMin; //saida
 
         System.out.println("Digite o quantidade de amostras:");
         Scanner entrada = new Scanner(System.in);
-        amostrasDoDia = entrada.nextDouble();
+        amostrasDoDia = entrada.nextInt();
 //primeira entrada do usuario
 
         System.out.println("Digite o tempo gasto para processar as amostras (em minutos):");    
@@ -19,19 +19,27 @@ public class laboratorio {
 //segunda entrada
 
 System.out.println("Digite o numero de resultados validos:");
-        resultadosValidos = entrada.nextDouble();
+        resultadosValidos = entrada.nextInt();
 //terceira entrada
 
-
-double percentualValidos1 = (resultadosValidos / amostrasDoDia) * 100; 
+double razaoValidos = ((double)resultadosValidos / amostrasDoDia);
+double percentualValidos1 = razaoValidos * 100;
 //calculo para o resultado do percentual de resultados validos
 
-        tempoMedioAmostras = amostrasDoDia / tempoDeProcessamento; //calculo para o resultado do tempo medio de amostras
-        resultadoTotal = tempoMedioAmostras;
-        System.out.println("Resultados processados por minuto: " + resultadoTotal); //saida
-        System.out.println("Percentual de resultados validos: " + percentualValidos1 + "%"); //saida
+        tempoMedioAmostras = amostrasDoDia / tempoDeProcessamento; 
+        resultadoProcessadosMin = tempoMedioAmostras; //calculo para o resultado do tempo medio de amostras
+        
+System.out.println("Aqui estão os resultados do dia do laboratorio:");
+
+        System.out.println("Amostras do dia: " + amostrasDoDia); //saida
+        System.out.printf("Tempo de processamento: %.2f minutos%n", tempoDeProcessamento); //saida
+        System.out.println("Resultados validos: " + resultadosValidos); //saida
+        System.out.printf("Tempo medio de amostras processadas por minuto: %.2f%n", tempoMedioAmostras); //saida
+        System.out.printf("Resultados processados por minuto: %.2f%n", resultadoProcessadosMin); //saida
+        System.out.printf("Percentual de resultados validos: %.2f%%%n", percentualValidos1); //saida
 
 
         entrada.close(); //finalização do scanner
     }
 }
+
